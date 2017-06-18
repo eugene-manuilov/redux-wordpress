5cf03a47cd549f3374818b738a90109f61fa0866
Eugene Manuilov <eugene.manuilov@get10up.com>
2017-06-19 00:12:54 +0300

    Updated createActions function to support args object

--- src/actions.js
+++ src/actions.js
@@ -52,123 +52,146 @@ const getErrorHandlerById = (dispatch, type, id, params) => (error) => {
 	});
 };
 
-export default function createActions(name, host, endpoints, namespace = 'wp/v2') {
+export default function createActions(name, host, endpoints, args = {}) {
+	let options = args;
+
+	// fallback for previous version of this function where the last param was
+	// for namespace argument
+	if (typeof args === 'string') {
+		options = { namespace: args };
+	}
+
 	const actions = {};
 	const normalizedHost = trimEnd(host, '/');
+	const namespace = options.namespace || 'wp/v2';
 
 	endpoints.forEach((endpoint) => {
 		const endpointName = upperFirst(endpoint);
 
-		actions[`fetch${endpointName}`] = (params = {}) => (dispatch) => {
-			const type = `@@wp/${name}/fetched/${endpoint}`;
+		if (options.fetch !== false) {
+			actions[`fetch${endpointName}`] = (params = {}) => (dispatch) => {
+				const type = `@@wp/${name}/fetched/${endpoint}`;
 
-			dispatch({
-				type: `@@wp/${name}/fetching/${endpoint}`,
-				params,
-			});
+				dispatch({
+					type: `@@wp/${name}/fetching/${endpoint}`,
+					params,
+				});
 
-			return fetchSingle(
-				`${normalizedHost}/${namespace}/${endpoint}?${qs(params)}`,
-				getSuccessHandler(dispatch, type, params),
-				getErrorHandler(dispatch, type, params),
-			);
-		};
+				return fetchSingle(
+					`${normalizedHost}/${namespace}/${endpoint}?${qs(params)}`,
+					getSuccessHandler(dispatch, type, params),
+					getErrorHandler(dispatch, type, params),
+				);
+			};
+		}
 
-		actions[`fetch${endpointName}Endpoint`] = (endpoint2, params = {}) => (dispatch) => {
-			const type = `@@wp/${name}/fetched/${endpoint}/${endpoint2}`;
+		if (options.fetchEndpoint !== false) {
+			actions[`fetch${endpointName}Endpoint`] = (endpoint2, params = {}) => (dispatch) => {
+				const type = `@@wp/${name}/fetched/${endpoint}/${endpoint2}`;
 
-			dispatch({
-				type: `@@wp/${name}/fetching/${endpoint}/${endpoint2}`,
-				params,
-			});
+				dispatch({
+					type: `@@wp/${name}/fetching/${endpoint}/${endpoint2}`,
+					params,
+				});
 
-			return fetchSingle(
-				`${normalizedHost}/${namespace}/${endpoint}/${endpoint2}?${qs(params)}`,
-				getSuccessHandler(dispatch, type, params),
-				getErrorHandler(dispatch, type, params),
-			);
-		};
+				return fetchSingle(
+					`${normalizedHost}/${namespace}/${endpoint}/${endpoint2}?${qs(params)}`,
+					getSuccessHandler(dispatch, type, params),
+					getErrorHandler(dispatch, type, params),
+				);
+			};
+		}
 
-		actions[`fetch${endpointName}ById`] = (id, params = {}) => (dispatch) => {
-			const type = `@@wp/${name}/fetched-by-id/${endpoint}`;
+		if (options.fetchById !== false) {
+			actions[`fetch${endpointName}ById`] = (id, params = {}) => (dispatch) => {
+				const type = `@@wp/${name}/fetched-by-id/${endpoint}`;
 
-			dispatch({
-				type: `@@wp/${name}/fetching-by-id/${endpoint}`,
-				id,
-				params,
-			});
+				dispatch({
+					type: `@@wp/${name}/fetching-by-id/${endpoint}`,
+					id,
+					params,
+				});
 
-			return fetchSingle(
-				`${normalizedHost}/${namespace}/${endpoint}/${id}?${qs(params)}`,
-				getSuccessHandlerById(dispatch, type, id, params),
-				getErrorHandlerById(dispatch, type, id, params),
-			);
-		};
+				return fetchSingle(
+					`${normalizedHost}/${namespace}/${endpoint}/${id}?${qs(params)}`,
+					getSuccessHandlerById(dispatch, type, id, params),
+					getErrorHandlerById(dispatch, type, id, params),
+				);
+			};
+		}
 
-		actions[`fetch${endpointName}EndpointById`] = (id, endpoint2, params = {}) => (dispatch) => {
-			const type = `@@wp/${name}/fetched-by-id/${endpoint}/${endpoint2}`;
+		if (options.fetchEndpointById !== false) {
+			actions[`fetch${endpointName}EndpointById`] = (id, endpoint2, params = {}) => (dispatch) => {
+				const type = `@@wp/${name}/fetched-by-id/${endpoint}/${endpoint2}`;
 
-			dispatch({
-				type: `@@wp/${name}/fetching-by-id/${endpoint}/${endpoint2}`,
-				id,
-				params,
-			});
+				dispatch({
+					type: `@@wp/${name}/fetching-by-id/${endpoint}/${endpoint2}`,
+					id,
+					params,
+				});
 
-			return fetchSingle(
-				`${normalizedHost}/${namespace}/${endpoint}/${id}/${endpoint2}?${qs(params)}`,
-				getSuccessHandlerById(dispatch, type, id, params),
-				getErrorHandlerById(dispatch, type, id, params),
-			);
-		};
+				return fetchSingle(
+					`${normalizedHost}/${namespace}/${endpoint}/${id}/${endpoint2}?${qs(params)}`,
+					getSuccessHandlerById(dispatch, type, id, params),
+					getErrorHandlerById(dispatch, type, id, params),
+				);
+			};
+		}
 
-		actions[`fetchAll${endpointName}`] = (params = {}) => (dispatch) => {
-			const type = `@@wp/${name}/fetched-all/${endpoint}`;
+		if (options.fetchAll !== false) {
+			actions[`fetchAll${endpointName}`] = (params = {}) => (dispatch) => {
+				const type = `@@wp/${name}/fetched-all/${endpoint}`;
 
-			dispatch({
-				type: `@@wp/${name}/fetching-all/${endpoint}`,
-				params,
-			});
+				dispatch({
+					type: `@@wp/${name}/fetching-all/${endpoint}`,
+					params,
+				});
 
-			return fetchAll(
-				`${normalizedHost}/${namespace}/${endpoint}`,
-				params,
-				getSuccessHandler(dispatch, type, params),
-				getErrorHandler(dispatch, type, params),
-			);
-		};
+				return fetchAll(
+					`${normalizedHost}/${namespace}/${endpoint}`,
+					params,
+					getSuccessHandler(dispatch, type, params),
+					getErrorHandler(dispatch, type, params),
+				);
+			};
+		}
 
-		actions[`fetchAll${endpointName}Endpoint`] = (endpoint2, params = {}) => (dispatch) => {
-			const type = `@@wp/${name}/fetched-all/${endpoint}/${endpoint2}`;
+		if (options.fetchAllEndpoint !== false) {
+			actions[`fetchAll${endpointName}Endpoint`] = (endpoint2, params = {}) => (dispatch) => {
+				const type = `@@wp/${name}/fetched-all/${endpoint}/${endpoint2}`;
 
-			dispatch({
-				type: `@@wp/${name}/fetching-all/${endpoint}/${endpoint2}`,
-				params,
-			});
+				dispatch({
+					type: `@@wp/${name}/fetching-all/${endpoint}/${endpoint2}`,
+					params,
+				});
 
-			return fetchAll(
-				`${normalizedHost}/${namespace}/${endpoint}/${endpoint2}`,
-				params,
-				getSuccessHandler(dispatch, type, params),
-				getErrorHandler(dispatch, type, params),
-			);
-		};
+				return fetchAll(
+					`${normalizedHost}/${namespace}/${endpoint}/${endpoint2}`,
+					params,
+					getSuccessHandler(dispatch, type, params),
+					getErrorHandler(dispatch, type, params),
+				);
+			};
+		}
 
-		actions[`fetchAll${endpointName}EndpointById`] = (id, endpoint2, params = {}) => (dispatch) => {
-			const type = `@@wp/${name}/fetched-all-by-id/${endpoint}/${endpoint2}`;
+		if (options.fetchAllEnpointById !== false) {
+			actions[`fetchAll${endpointName}EndpointById`] = (id, endpoint2, params = {}) => (dispatch) => {
+				const type = `@@wp/${name}/fetched-all-by-id/${endpoint}/${endpoint2}`;
 
-			dispatch({
-				type: `@@wp/${name}/fetching-all-by-id/${endpoint}/${endpoint2}`,
-				id,
-				params,
-			});
+				dispatch({
+					type: `@@wp/${name}/fetching-all-by-id/${endpoint}/${endpoint2}`,
+					id,
+					params,
+				});
 
-			return fetchAll(
-				`${normalizedHost}/${namespace}/${endpoint}/${id}/${endpoint2}`,
-				params,
-				getSuccessHandlerById(dispatch, type, id, params),
-				getErrorHandlerById(dispatch, type, id, params),
-			);
-		};
+				return fetchAll(
+					`${normalizedHost}/${namespace}/${endpoint}/${id}/${endpoint2}`,
+					params,
+					getSuccessHandlerById(dispatch, type, id, params),
+					getErrorHandlerById(dispatch, type, id, params),
+				);
+			};
+		}
 	});
 
 	return actions;
