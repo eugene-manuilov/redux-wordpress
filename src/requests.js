25123721b8f2ee6939bdfcc9c262806fcbfec410
Eugene Manuilov <eugene.manuilov@get10up.com>
2017-06-19 00:30:57 +0300

    Added ability to select what requests need to be generated

--- src/requests.js
+++ src/requests.js
@@ -1,20 +1,48 @@
 import { qs, upperFirst, trimEnd, requestAll, requestSingle } from './helpers';
 
-export default function createRequests(host, endpoints, namespace = 'wp/v2') {
+export default function createRequests(host, endpoints, args = {}) {
+	let options = args;
+
+	// fallback for previous version of this function where the last param was
+	// for namespace argument
+	if (typeof args === 'string') {
+		options = { namespace: args };
+	}
+
 	const requests = {};
+	const namespace = options.namespace || 'wp/v2';
 
 	endpoints.forEach((endpoint) => {
 		const normalizedURL = trimEnd(host, '/');
 		const endpointName = upperFirst(endpoint);
 
-		requests[`request${endpointName}`] = (params = {}) => requestSingle(`${normalizedURL}/${namespace}/${endpoint}?${qs(params)}`);
-		requests[`request${endpointName}Endpoint`] = (endpoint2, params = {}) => requestSingle(`${normalizedURL}/${namespace}/${endpoint}/${endpoint2}?${qs(params)}`);
-		requests[`request${endpointName}ById`] = (id, params = {}) => requestSingle(`${normalizedURL}/${namespace}/${endpoint}/${id}?${qs(params)}`);
-		requests[`request${endpointName}EndpointById`] = (id, endpoint2, params = {}) => requestSingle(`${normalizedURL}/${namespace}/${endpoint}/${id}/${endpoint2}?${qs(params)}`);
+		if (options.request !== false) {
+			requests[`request${endpointName}`] = (params = {}) => requestSingle(`${normalizedURL}/${namespace}/${endpoint}?${qs(params)}`);
+		}
 
-		requests[`requestAll${endpointName}`] = (params = {}) => requestAll(`${normalizedURL}/${namespace}/${endpoint}`, params);
-		requests[`requestAll${endpointName}Endpoint`] = (endpoint2, params = {}) => requestAll(`${normalizedURL}/${namespace}/${endpoint}/${endpoint2}`, params);
-		requests[`requestAll${endpointName}EndpointById`] = (id, endpoint2, params = {}) => requestAll(`${normalizedURL}/${namespace}/${endpoint}/${id}/${endpoint2}`, params);
+		if (options.requestEndpoint !== false) {
+			requests[`request${endpointName}Endpoint`] = (endpoint2, params = {}) => requestSingle(`${normalizedURL}/${namespace}/${endpoint}/${endpoint2}?${qs(params)}`);
+		}
+
+		if (options.requestById !== false) {
+			requests[`request${endpointName}ById`] = (id, params = {}) => requestSingle(`${normalizedURL}/${namespace}/${endpoint}/${id}?${qs(params)}`);
+		}
+
+		if (options.requestEndpointById !== false) {
+			requests[`request${endpointName}EndpointById`] = (id, endpoint2, params = {}) => requestSingle(`${normalizedURL}/${namespace}/${endpoint}/${id}/${endpoint2}?${qs(params)}`);
+		}
+
+		if (options.requestAll !== false) {
+			requests[`requestAll${endpointName}`] = (params = {}) => requestAll(`${normalizedURL}/${namespace}/${endpoint}`, params);
+		}
+
+		if (options.requestAllEndpoint !== false) {
+			requests[`requestAll${endpointName}Endpoint`] = (endpoint2, params = {}) => requestAll(`${normalizedURL}/${namespace}/${endpoint}/${endpoint2}`, params);
+		}
+
+		if (options.requestAllEndpointById !== false) {
+			requests[`requestAll${endpointName}EndpointById`] = (id, endpoint2, params = {}) => requestAll(`${normalizedURL}/${namespace}/${endpoint}/${id}/${endpoint2}`, params);
+		}
 	});
 
 	return requests;
